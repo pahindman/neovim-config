737e5377594c3547caa9430ebb9d30cb41c7b0ea
Phil Hindman <pahindman@user.noreply.github.com>
2024-06-05 23:21:08 -0500

    Gracefully handle missing lspconfig plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,5 +4,12 @@ vim.opt.runtimepath:append('~/.vim/after')
 vim.o.packpath = vim.o.runtimepath
 vim.cmd('source ~/.vimrc')
 
-require('lspconfig').clangd.setup{}
-require('lspconfig').rust_analyzer.setup{}
+-- Try to load the lspconfig plugin.  If that works then set up the LSP
+-- servers, otherwise print a message.
+local lspconfig, _ = pcall(require, 'lspconfig')
+if lspconfig then
+  require("lspconfig").clangd.setup{}
+  require("lspconfig").rust_analyzer.setup{}
+else
+  print('init.lua: lspconfig not found')
+end
